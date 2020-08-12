eb67f98d7ea2166b9c16257e1a4e140270d494c9
Sangjin <sjchoi@casys.kaist.ac.kr>
2020-08-12 14:40:25 +0900

    Reset all

---
+++ reset_all.sh
@@ -0,0 +1,4 @@
+sudo ipmitool -I lan -H 143.248.188.178 -U ADMIN -P calab4406! power reset
+sudo ipmitool -I lan -H 143.248.188.84 -U ADMIN -P calab4406! power reset
+sudo ipmitool -I lan -H 143.248.135.98 -U ADMIN -P calab4406! power reset
+sudo ipmitool -I lan -H 143.248.188.60 -U calab -P calab4406! power reset
