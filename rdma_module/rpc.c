7c5916200e5980894c789f771bb892a91617d1e6
sangjin <sjchoi>
2020-08-16 23:04:10 +0900

    async done pointer

--- rdma_module/rpc.c
+++ rdma_module/rpc.c
@@ -1558,11 +1558,13 @@ static int __rpc_handle_replicate_mem(void *args)
 		}
 
 		done = 0;
-		ret = rmm_evict_async(dest_nid, &addr_list, nr_pages, done);
+		ret = rmm_evict_async(dest_nid, &addr_list, nr_pages, &done);
 		//ret = rmm_evict(dest_nid, &addr_list, nr_pages);
 
+		/*
 		while (!(ret = done))
 			cpu_relax();
+		*/
 
 		list_for_each_safe(pos, n, &addr_list) {
 			ei = list_entry(pos, struct evict_info, next);
